d62034040b1913422b90f1b4aef960775130a2aa
Lorenzo Gatti <lorenzo.gatti@alpiq.com>
2020-06-30 11:11:31 +0200

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+role_arn = "arn:aws:iam::<account-id>:role/<role-name>"
---
+++ variables.tf
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------
+# Variables
+# --------------------------------------------------------------
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+variable "role_arn" {
+  type    = string
+}
