9a3f21f7094ef1612ceb45256cc3763ad05f9a14
Lorenzo Gatti <lorenzo.gatti@alpiq.com>
2020-07-06 13:50:59 +0200

    Fix content generic unicorn service lambda

--- lambdas/generic-unicorn-service/app.py
+++ lambdas/generic-unicorn-service/app.py
@@ -1,42 +1,29 @@
-import boto3
-from botocore.config import Config
 import json
 import os
 import random
 import time
 
 SERVICE_NAME = os.environ['SERVICE_NAME']
-QUEUE_URL = os.environ['QUEUE_URL']
 
-config = Config(connect_timeout=5, read_timeout=5, retries={'max_attempts': 1})
-sqs = boto3.client('sqs', config=config)
+def process(event):
+    print('{} process: {}'.format(SERVICE_NAME, event))
+    message_id = event['MessageId']
 
-def i_am_not_available(event):
-    return random.choice([True, False])
+    # will fail randomly to show the Amazon SNS redelivery feature
+    if random.choice([True, False]):
+        print("--------------------------------------")
+        print("{{'msg-id': '{}', 'status': 'FAILED'}}".format(message_id))
+        print("--------------------------------------")
+        raise Exception("I'm failing randomly...")
+
+    print("+++++++++++++++++++++++++++++++++++++++++")
+    print("{{'msg-id': '{}', 'status': 'PROCESSED'}}".format(message_id))
+    print("+++++++++++++++++++++++++++++++++++++++++")
+
 
 def lambda_handler(event, context):
     print('{} received event: {}'.format(SERVICE_NAME, json.dumps(event)))
 
-    # we only send a quote, if we are available at that time
-    if i_am_not_available(event):
-        return
-
-    # if we send a quote, we wait between 0 and 60 seconds to mimic the quote computation
-    time.sleep(random.randint(0,60))
-
-    # send the response to the quotes response queue
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-    response_message = json.dumps({
-        'responder': SERVICE_NAME,
-        'rfq-id': message['rfq-id'],
-        'quote': random.randint(0,100)
-    })
-
-    response = sqs.send_message(
-        QueueUrl = QUEUE_URL,
-        MessageBody = response_message
-    )
-
-    print('sent SQS message: {}'.format(response_message))
-
-    return
+    # we set the 'BatchSize' to 1
+    request_body = json.loads(event['Records'][0]['body'])
+    process(request_body)
